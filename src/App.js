4e906049f5b1910e7852d05c21e3a7ac6d945461
Irene R <irene@smallwondermedia.com>
2019-06-13 14:46:04 -0400

    map component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import './App.css';
 import axios from 'axios';
-import Park from "./Park/Park";
+import Park from "./components/Park/Park";
 
 function App() {
 

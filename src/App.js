1f3e1bd820b7224413ec1f42852d7ea5e6fcfcd4
Irene R <irene@smallwondermedia.com>
2019-06-13 20:47:52 -0400

    react-svg-tooltip attempt

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import axios from 'axios';
 import Park from "./components/Park/Park";
 import Map from "./components/Map/Map.js";
+import { Tooltip } from 'react-svg-tooltip';
 
 function App() {
 
@@ -19,8 +20,29 @@ function App() {
 
     // need loading animation since API is slow
 
+    const circleRef = React.createRef();
+
   return (
     <div className="App">
+
+    <svg viewBox="0 0 100 100">
+        <circle ref={circleRef} cx={50} cy={50} r={10} fill="steelblue" />
+        <Tooltip triggerRef={circleRef}>
+          <rect
+            x={2}
+            y={2}
+            width={10}
+            height={5}
+            rx={0.5}
+            ry={0.5}
+            fill="black"
+          />
+          <text x={5} y={5} fontSize={2} fill="white">
+            Yay!
+          </text>
+        </Tooltip>
+      </svg>
+
 
         <div>
             Click a state to see its national parks and trails
